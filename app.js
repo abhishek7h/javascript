d79e7d18064ecd30704390a6480f499ec714a838
Abhishek Hari <abhishek18h@gmail.com>
2024-03-28 21:28:52 +0530

    initial commit + backing up sm

---
+++ app.js
@@ -0,0 +1,93 @@
+// 1. Deposit money
+// 2. Ask number of lines to bet on
+// 3. Collect bet amount
+// 4. Spin machine
+// 5. Check if the user won
+// 6. Give user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter the amount you want to deposit: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt(
+      "Enter the number of lines you want to bet on (1-3): "
+    );
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const getBet = (balance) => {
+  while (true) {
+    const bet = prompt("Enter the amount you want to bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (
+      isNaN(numberBet) ||
+      numberBet <= 0 ||
+      numberBet > balance / numberOfLines
+    ) {
+      console.log("Invalid bet, try again.");
+    } else {
+      return numberBet;
+    }
+  }
+};
+
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOL_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [[], [], []];
+  for (let i = 0; i < COLS; i++) {
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+    }
+  }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
